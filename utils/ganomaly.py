0bddb0e4ce9644e977bc1abd1793caf856caebd9
Purna Chandu Challagulla <challagullapurnachandu@gmail.com>
2025-01-24 17:27:25 +0530

    Add files via upload

---
+++ utils/ganomaly.py
@@ -0,0 +1,12 @@
+class GANomaly:
+    def __init__(self, lr):
+        # Define encoder, generator, and discriminator
+        pass
+
+    def train(self, train_loader, epochs):
+        # Training logic for GANomaly
+        pass
+
+    def test(self, test_loader):
+        # Testing logic for GANomaly
+        pass
---
+++ utils/multi_kd.py
@@ -0,0 +1,12 @@
+class MultiKD:
+    def __init__(self, lr):
+        # Define source and cloner networks
+        pass
+
+    def train(self, train_loader, epochs):
+        # Train Multi-KD model
+        pass
+
+    def test(self, test_loader):
+        # Test Multi-KD model
+        pass
